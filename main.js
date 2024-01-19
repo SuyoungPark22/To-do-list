e5eabfc8c3ddf42fc87b37ab342617a55569be8d
unknown <suyoung0222@snu.ac.kr>
2024-01-19 14:14:25 +0900

    enter key to add list

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 let inputField = document.getElementById('inputField');
-let addToDo = document.getElementById('addToDo');
+let addButton = document.getElementById('addButton');
 let toDoList = document.getElementById('toDoList');
- 
-addToDo.addEventListener('click', function(){
+let checkboxes = document.getElementsByClassName('checkbox');
+
+// for(var i=0; i<checkboxes.length; i++){
+//     var checkbox = checkboxes.item(i);
+//     checkbox.addEventListener('click', () => {
+//         if (checkbox.checked)
+//             toDoList.children[i].querySelector('label').style.textDecoration = 'line-through';
+//             // checkbox.nextSibling.style.textDecoration = 'line-through';
+//         else
+//             toDoList.children[i].querySelector('label').style.textDecoration = 'none';
+//             // checkbox.nextSibling.style.textDecoration = 'none';
+//     })
+// }
+
+inputField.addEventListener('keyup', (event) => {
+    if (event.code == 'Enter')
+        addToDo();
+})
+
+addButton.addEventListener('click', () => {
+    addToDo();
+})
+
+function addToDo(){
     if (!inputField.value)
-        alert("Please write your to-do.");
+        alert("Please write your to-do.")
     else
     {
         var list = document.createElement('p');
         var checkbox = document.createElement('input');
         var label = document.createElement('label');
         
+        list.id = inputField.value;
         checkbox.type = "checkbox";
-        checkbox.id = inputField.value;
-        checkbox.class = "checkbox";
+        checkbox.id = inputField.value;  
+        checkbox.setAttribute("class", "checkbox");
         label.for = inputField.value;
         label.innerText = inputField.value;
         
@@ -24,4 +47,5 @@ addToDo.addEventListener('click', function(){
 
         inputField.value = '';
     }
-})
+}
+
