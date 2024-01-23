6bf6be6db90f4031b13c182743e9b78e7995de01
unknown <suyoung0222@snu.ac.kr>
2024-01-23 23:50:21 +0900

    delButton

--- main.js
+++ main.js
@@ -3,18 +3,6 @@ let addButton = document.getElementById('addButton');
 let toDoList = document.getElementById('toDoList');
 let checkboxes = document.getElementsByClassName('checkbox');
 
-// for(var i=0; i<checkboxes.length; i++){
-//     var checkbox = checkboxes.item(i);
-//     checkbox.addEventListener('click', () => {
-//         if (checkbox.checked)
-//             toDoList.children[i].querySelector('label').style.textDecoration = 'line-through';
-//             // checkbox.nextSibling.style.textDecoration = 'line-through';
-//         else
-//             toDoList.children[i].querySelector('label').style.textDecoration = 'none';
-//             // checkbox.nextSibling.style.textDecoration = 'none';
-//     })
-// }
-
 inputField.addEventListener('keyup', (event) => {
     if (event.code == 'Enter')
         addToDo();
@@ -25,12 +13,21 @@ addButton.addEventListener('click', () => {
 })
 
 toDoList.addEventListener('change', (event) => {
-    var button = event.target;
+    var checkbox = event.target;
     var text = event.target.nextElementSibling;
-    if (button.checked == true)
+    if (checkbox.checked == true)
         text.style.textDecoration = 'line-through';
     else
         text.style.textDecoration = 'none';
+    event.stopPropagation();
+}, false)
+
+toDoList.addEventListener('click', (event) => {
+    if (event.target.getAttribute('class') == 'delButton'){
+        var delButton = event.target;
+        var list = delButton.parentElement;
+        list.remove();
+    }
     event.stopPropagation();
 }, false)
 
@@ -42,14 +39,18 @@ function addToDo(){
         var list = document.createElement('p');
         var checkbox = document.createElement('input');
         var text = document.createElement('span');
+        var delButton = document.createElement('button');
         
         list.id = inputField.value;
         checkbox.type = "checkbox";
         checkbox.id = inputField.value;
         text.innerText = inputField.value;
+        delButton.setAttribute('class', 'delButton');
+        delButton.innerText = "x";
         
         list.appendChild(checkbox);
         list.appendChild(text);
+        list.appendChild(delButton);
 
         toDoList.appendChild(list);
 
